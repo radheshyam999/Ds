04dbaf8a642e2068943a2cbfc4608fab09466764
radheshyam999 <radheshyamdubey999@gmail.com>
2019-06-27 11:39:43 +0530

    some file  created

--- Java/ArrayList/myArrayList.java
+++ Java/ArrayList/myArrayList.java
@@ -1,7 +1,20 @@
+import java.util.Map;
+import java.util.Stack;
+import java.util.WeakHashMap;
+
+import com.sun.javafx.css.CascadingStyle;
 public  class myArrayList
  {
-   
+   public static void main(String... ar) {
 
+    WeakHashMap <String, String> wm = new WeakHashMap<>();
+    wm.put("html", "HYPER text Markup Language");
+    wm.put("css", "Cascading Style sheet" );
+    for (Map.Entry m:wm.entrySet() ) {
+        System.out.println(m.getValue());
+    }
+   }
 
+  
 
  }
